2514b950fbc503beeebd733de50bb7d6f6cf22b6
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2022-08-10 01:15:30 -0400

    fix: axios error log

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -19,6 +19,8 @@ module.exports = {
           { name: "Recursos Ativos", value: `${guild.features.join(" ")}`}
         ]
       }]
-    })
+    }).catch(err => {
+      guild.client.logger.error(err);
+    });
   }
 }
