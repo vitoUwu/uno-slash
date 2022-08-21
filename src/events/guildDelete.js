e6481e1312d361623edc8bce8591190745fddce0
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2022-08-21 13:42:43 -0400

    feat: guildDelete logging

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -3,7 +3,7 @@ const { Guild, Colors } = require("discord.js");
 const { loggerUrl } = require("../../config.json");
 
 module.exports = {
-  name: "guildCreate",
+  name: "guildDelete",
   /**
    * @param {Guild} guild
    */
@@ -18,8 +18,8 @@ module.exports = {
           { name: "Língua", value: `${guild.preferredLocale || "No preferred locale"}`, inline: true },
           { name: "Recursos Ativos", value: `${guild.features.join(" ") || "No features"}`}
         ],
-        color: Colors.Green
-      }],
+        color: Colors.Red
+      }]
     }).catch(err => {
       guild.client.logger.error(err);
     });
