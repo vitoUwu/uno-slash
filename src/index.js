02b3e796b5a22551af3969c1cfcf5a531ce60380
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-04-04 09:01:18 -0400

    feat: sharding

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
-import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { ShardingManager } from "discord.js";
 import "dotenv/config";
-import { deployCommands, loadCommands } from "./handlers/commands.js";
-import { loadAndListenEvents } from "./handlers/event.js";
-import { postStatus, updateActivity } from "./utils/functions.js";
 import { logger } from "./utils/logger.js";
 
-(async () => {
-  const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
-    partials: [Partials.Channel, Partials.GuildMember],
-  });
+const manager = new ShardingManager("./src/uno.js", {
+  token: process.env.DISCORD_TOKEN,
+  totalShards: "auto",
+});
 
-  await loadAndListenEvents(client);
-  await loadCommands();
-  await client.login(process.env.DISCORD_TOKEN);
-  await client.application.fetch();
-  logger.info(`Logado como ${client.user.tag}`);
-  await deployCommands(client);
+manager.on("shardCreate", (shard) => logger.info(`Launched shard ${shard.id}`));
 
-  updateActivity(client);
-  await postStatus(client.guilds.cache.size, client.user.id);
-
-  setTimeout(async () => {
-    updateActivity(client);
-    await postStatus(client.guilds.cache.size, client.user.id);
-  }, 60000 * 10);
-})();
+manager.spawn();
---
+++ src/uno.js
@@ -0,0 +1,29 @@
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { deployCommands, loadCommands } from "./handlers/commands.js";
+import { loadAndListenEvents } from "./handlers/event.js";
+import { postStatus, updateActivity } from "./utils/functions.js";
+import { logger } from "./utils/logger.js";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+  partials: [Partials.Channel, Partials.GuildMember],
+});
+
+(async () => {
+  const start = Date.now();
+  await loadAndListenEvents(client);
+  await loadCommands();
+  await client.login(process.env.DISCORD_TOKEN);
+  await client.application.fetch();
+  logger.info(`Logado como ${client.user.tag}`);
+  await deployCommands(client);
+  logger.info(`Inicialização realizada em ${Date.now() - start}ms`);
+
+  updateActivity(client);
+  await postStatus(client);
+
+  setInterval(async () => {
+    updateActivity(client);
+    await postStatus(client);
+  }, 60000 * 10);
+})();
